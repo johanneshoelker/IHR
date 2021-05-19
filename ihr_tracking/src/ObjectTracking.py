461b45ec710ceacea9125887e6db9b59176027b2
johanneshoelker_vm <johannes.hoelker@uni-kassel.de>
2021-05-19 16:37:28 +0200

    Week 3 OpenCV Tracking Solution by Ruda

--- ihr_tracking/src/ObjectTracking.py
+++ ihr_tracking/src/ObjectTracking.py
@@ -112,21 +112,105 @@ class CentroidTracker(object):
         del self.objects[objectID]
         del self.disappeared[objectID]
 
-    def connectObj(self, inputCentroids):
-
-        # grab the set of object IDs and corresponding centroids
-        objectIDs = list(self.objects.keys())
-        objectCentroids = list(self.objects.values())
-
-        #TODO:Use the Euclidean distance to connect the current object
-        #to be tracked with an existing object. If there are more or less
-        #new objects than old objects, these must be registered or deregistered
-        #4)
+    def connectObj(self, inputCentroids):   #new: inputCentroids
+            # grab the set of object IDs and corresponding inputCentroids
+            objectIDs = list(self.objects.keys())
+            objectCentroids = list(self.objects.values())
 
 
+			# compute the distance between each pair of object
+			# centroids and input centroids, respectively -- our
+			# goal will be to match an input centroid to an existing
+			# object centroid
+            D = dist.cdist(np.array(objectCentroids), inputCentroids)
 
-        return self.objects
+			# in order to perform this matching we must (1) find the
+			# smallest value in each row and then (2) sort the row
+			# indexes based on their minimum values so that the row
+			# with the smallest value as at the *front* of the index
+			# list
+            rows = D.min(axis=1).argsort()
+            #rospy.loginfo(rows)
+			# next, we perform a similar process on the columns by
+			# finding the smallest value in each column and then
+			# sorting using the previously computed row index list
+            cols = D.argmin(axis=1)[rows]
+            #rospy.loginfo(cols)
+			# in order to determine if we need to update, register,
+			# or deregister an object we need to keep track of which
+			# of the rows and column indexes we have already examined
 
+            usedRows = set()
+
+            usedCols = set()
+
+			# loop over the combination of the (row, column) index
+			# tuples
+
+            for (row, col) in zip(rows, cols):
+				# if we have already examined either the row or
+				# column value before, ignore it
+				# val
+
+                if row in usedRows or col in usedCols:
+                    continue
+
+				# otherwise, grab the object ID for the current row,
+				# set its new centroid, and reset the disappeared
+				# counter
+                objectID = objectIDs[row]
+                #rospy.loginfo(self.objects[objectID])
+                self.objects[objectID] = inputCentroids[col]
+                self.disappeared[objectID] = 0
+
+				# indicate that we have examined each of the row and
+				# column indexes, respectively
+                usedRows.add(row)
+                usedCols.add(col)
+
+			# compute both the row and column index we have NOT yet
+			# examined
+
+            unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+            rospy.loginfo(self.objects[objectID])
+            unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+
+			# in the event that the number of object centroids is
+			# equal or greater than the number of input centroids
+			# we need to check and see if some of these objects have
+			# potentially disappeared
+
+            if D.shape[0] >= D.shape[1]:
+				# loop over the unused row indexes
+
+                for row in unusedRows:
+					# grab the object ID for the corresponding row
+					# index and increment the disappeared counter
+
+                    objectID = objectIDs[row]
+
+                    self.disappeared[objectID] += 1
+
+					# check to see if the number of consecutive
+					# frames the object has been marked "disappeared"
+					# for warrants deregistering the object
+
+                    if self.disappeared[objectID] > self.maxDisappeared:
+
+                        self.deregister(objectID)
+
+			# otherwise, if the number of input centroids is greater
+			# than the number of existing object centroids we need to
+			# register each new input centroid as a trackable object
+
+            else:
+
+                for col in unusedCols:
+
+                    self.register(inputCentroids[col])
+
+		    # return the set of trackable objects
+            return self.objects
 
     def update(self, rects):
         # check to see if the list of input bounding box rectangles
